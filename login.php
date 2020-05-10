12d4a9e1cd8e7bfcd5ea726bec1e14dee14c5cb2
samo <samo@it-tf.local>
2020-05-10 16:53:11 +0200

    basic2

--- login.php
+++ login.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meine Filme | Filmtagebuch</title>
+    <title>Login | Filmtagebuch</title>
 </head>
 <body>
     <header>
-            <?php
+        <?php
                 $nav = file_get_contents("./nav.html")
                 echo $nav
-            ?>
+        ?>
     </header>
 </body>
 </html>
--- suche.php
+++ suche.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meine Filme | Filmtagebuch</title>
+    <title>Suche | Filmtagebuch</title>
 </head>
 <body>
-    <header>
+        <header>
             <?php
                 $nav = file_get_contents("./nav.html")
                 echo $nav
             ?>
-    </header>
+        </header>
 </body>
 </html>
